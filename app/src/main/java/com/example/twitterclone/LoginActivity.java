37d80be943840692a182d21a585f2b24efdb70ed
hitesh <hitesh9697@gmail.com>
2021-04-23 20:33:40 +0530

    login and sign up functionality added

--- app/src/main/java/com/example/twitterclone/LoginActivity.java
+++ app/src/main/java/com/example/twitterclone/LoginActivity.java
@@ -2,15 +2,19 @@ package com.example.twitterclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.twitterclone.databinding.ActivityLoginBinding;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +23,34 @@ public class LoginActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         setTitle("Login");
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setMessage("Logging in");
+        binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(binding.editTextLoginUsername.getText().toString(), binding.editTextLoginPassword.getText().toString());
+            }
+        });
 
         binding.buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        progressDialog.show();
+        ParseUser.logInInBackground(username, password, (parseUser, e) -> {
+            progressDialog.dismiss();
+            if (parseUser != null) {
+                Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                ParseUser.logOut();
+                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
--- app/src/main/java/com/example/twitterclone/SignUpActivity.java
+++ app/src/main/java/com/example/twitterclone/SignUpActivity.java
@@ -2,10 +2,15 @@ package com.example.twitterclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.twitterclone.databinding.ActivitySignUpBinding;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -18,5 +23,40 @@ public class SignUpActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         setTitle("Sign Up");
+        ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this);
+        progressDialog.setMessage("Signing Up");
+
+        binding.buttonNewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (binding.editTextSignUpUsername.getText().toString().equals("") && binding.editTextSignUpPassword.getText().toString().equals("")){
+                    Toast.makeText(SignUpActivity.this, "Username/Password is required", Toast.LENGTH_SHORT).show();
+                } else if (!binding.editTextSignUpPassword.getText().toString().equals(binding.editTextSignUpConfirmPassword.getText().toString())
+                        && !binding.editTextSignUpPassword.getText().toString().equals("")) {
+                    Toast.makeText(SignUpActivity.this, "Password does not match. Please try again!", Toast.LENGTH_SHORT).show();
+                } else {
+                    ParseUser user = new ParseUser();
+                    // Set the user's username and password, which can be obtained by a forms
+                    user.setUsername(binding.editTextSignUpUsername.getText().toString());
+                    user.setPassword(binding.editTextSignUpPassword.getText().toString());
+                    progressDialog.show();
+                    user.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            progressDialog.dismiss();
+                            if (e == null) {
+                                Toast.makeText(SignUpActivity.this, "Sign Up successful", Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else {
+                                ParseUser.logOut();
+                                Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
     }
 }
